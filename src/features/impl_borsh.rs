e420e639f7831f3cffc80587d8c6962afbae99ed
cybersoulK <102260392+cybersoulK@users.noreply.github.com>
2024-07-25 22:21:54 -0700

    .

--- src/features/impl_borsh.rs
+++ src/features/impl_borsh.rs
@@ -1,5 +1,5 @@
 
-macro_rules! impl_to_from_array {
+macro_rules! impl_to_from_array2 {
     ($type:ident, $array_type:ty) => {
 
         impl $type {
@@ -46,12 +46,12 @@ macro_rules! impl_borsh {
 mod f32 {
     use crate::{Affine2, Affine3A, Mat2, Mat3, Mat3A, Mat4, Quat, Vec2, Vec3, Vec3A, Vec4};
     
-    impl_to_from_array!(Affine2, [f32; 6]);
-    impl_to_from_array!(Affine3A, [f32; 12]);
-    impl_to_from_array!(Mat2, [f32; 4]);
-    impl_to_from_array!(Mat3, [f32; 9]);
-    impl_to_from_array!(Mat3A, [f32; 9]);
-    impl_to_from_array!(Mat4, [f32; 16]);
+    impl_to_from_array2!(Affine2, [f32; 6]);
+    impl_to_from_array2!(Affine3A, [f32; 12]);
+    impl_to_from_array2!(Mat2, [f32; 4]);
+    impl_to_from_array2!(Mat3, [f32; 9]);
+    impl_to_from_array2!(Mat3A, [f32; 9]);
+    impl_to_from_array2!(Mat4, [f32; 16]);
 
 
     impl_borsh!(Affine2, [f32; 6]);
@@ -71,11 +71,11 @@ mod f32 {
 mod f64 {
     use crate::{DAffine2, DAffine3, DMat2, DMat3, DMat4, DQuat, DVec2, DVec3, DVec4};
 
-    impl_to_from_array!(DAffine2, [f64; 6]);
-    impl_to_from_array!(DAffine3, [f64; 12]);
-    impl_to_from_array!(DMat2, [f64; 4]);
-    impl_to_from_array!(DMat3, [f64; 9]);
-    impl_to_from_array!(DMat4, [f64; 16]);
+    impl_to_from_array2!(DAffine2, [f64; 6]);
+    impl_to_from_array2!(DAffine3, [f64; 12]);
+    impl_to_from_array2!(DMat2, [f64; 4]);
+    impl_to_from_array2!(DMat3, [f64; 9]);
+    impl_to_from_array2!(DMat4, [f64; 16]);
 
 
     impl_borsh!(DAffine2, [f64; 6]);
